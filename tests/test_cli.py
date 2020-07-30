d387e2411fd9e0dc7ff00ff813592515e335cffe
Stefan Hagen <stefan@hagen.link>
2020-07-30 15:21:56 +0200

    Update test_cli.py

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,3 +3,10 @@
 import pytest  # type: ignore
 
 import json_schema_compress.cli as cli
+
+
+def test_main_ok_empty_array(capsys):
+    empty = '[]'
+    assert cli.main(empty) is None
+    out, err = capsys.readouterr()
+    assert out.strip() == str(None)
