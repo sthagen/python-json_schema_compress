616f7378d2cb3719e5db99289b4338041a6bedff
Stefan Hagen <stefan@hagen.link>
2020-07-30 15:47:22 +0200

    Added negative functional API test case
    And some naming fixes

--- tests/test_json_schema_compress.py
+++ tests/test_json_schema_compress.py
@@ -4,11 +4,18 @@ import pytest  # type: ignore
 
 import json_schema_compress.json_schema_compress as jsc
 
-def test_walk_ok_empty_array():
+def test_process_ok_empty_array():
     job = ['[]']
     assert jsc.process(job) == job[0]
 
 
-def test_walk_ok_empty_object():
+def test_process_ok_empty_object():
     job = ['{}']
     assert jsc.process(job) == job[0]
+
+
+def test_process_nok_wrong_type_string():
+    bad = ["bad"]
+    message = r"Expecting value: line 1 column 1 \(char 0\)"
+    with pytest.raises(json.decoder.JSONDecodeError, match=message):
+        jsc.process(bad)
