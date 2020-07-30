a567d3572682a9fe509508928df20b0021a5dab2
Stefan Hagen <stefan@hagen.link>
2020-07-30 15:39:23 +0200

    Fixed negative command line API test case

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=missing-docstring,unused-import,reimported
+import json
 import pytest  # type: ignore
 
 import json_schema_compress.cli as cli
@@ -21,6 +22,6 @@ def test_main_ok_empty_object(capsys):
 
 def test_main_nok_wrong_type_string():
     bad = "bad"
-    message = r"object of type 'type' has no len\(\)"
-    with pytest.raises(TypeError, match=message):
+    message = r"Expecting value: line 1 column 1 \(char 0\)"
+    with pytest.raises(json.decoder.JSONDecodeError, match=message):
         cli.main(bad)
