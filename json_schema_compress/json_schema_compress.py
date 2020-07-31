25a3d1825185f709674e7e5bb9409a9eb66d707d
Stefan Hagen <stefan@hagen.link>
2020-07-31 12:43:33 +0200

    Refactoring of visitor

--- json_schema_compress/json_schema_compress.py
+++ json_schema_compress/json_schema_compress.py
@@ -4,14 +4,30 @@
 import json
 
 
+def is_description(key):
+    """Simple matcher."""
+    return key == "description"
+
+
+def keep_first_sentence_only(value):
+    """Simple compressor."""
+    if '.' in value:
+        return f"{value.split('.', 1)[0]}."
+    if '\n' in value:
+        return value.split('\n', 1)[0]
+    return value
+
+
+def visit(predicate, compressor, tree):
+    """Naive initial tree visitor compressing values based on predicate."""
+    for key in tree:
+        if predicate(key):
+            tree[key] = compressor(tree[key])
+    return tree
+
+
 def process(data):
     """Model documentation ... TODO."""
-    hosted = json.loads(data[0])
-    for key in hosted:
-        if key == "description":
-            value = hosted[key]
-            if '.' in value:
-                hosted[key] = f"{value.split('.', 1)[0]}."
-            elif '\n' in value:
-                hosted[key] = value.split('\n', 1)[0]
-    return json.dumps(hosted)
+    tree = json.loads(data[0])
+    visit(tree)
+    return json.dumps(tree)
