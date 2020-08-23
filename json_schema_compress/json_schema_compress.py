407ba9997d2ff2ecf4267e21e1955e7db5f62738
Stefan Hagen <stefan@hagen.link>
2020-08-23 14:28:53 +0200

    Added experiemental non-working path extractor seed

--- json_schema_compress/json_schema_compress.py
+++ json_schema_compress/json_schema_compress.py
@@ -9,12 +9,23 @@ def is_description(key):
     return key == "description"
 
 
+def anything(key):
+    """Simple wildcard."""
+    return key
+
+
 def keep_first_sentence_only(value):
     """Simple compressor."""
     if '.' in value:
         return f"{value.split('.', 1)[0]}."
     if '\n' in value:
         return value.split('\n', 1)[0]
+    return value
+
+
+def printer(value):
+    """Simple printer."""
+    print(value)
     return value
 
 
@@ -30,6 +41,21 @@ def visit(predicate, compressor, tree):
         else:
             for value in tree:
                 visit(predicate, compressor, value)
+
+
+def extract_paths(predicate, extractor, tree):
+    """Initial tree visitor implementation extracting paths based on predicate."""
+    if isinstance(tree, (dict, list)):
+        if isinstance(tree, dict):
+            for key, value in tree.items():
+                if predicate(key):
+                    extractor(key)
+                    tree[key] = value
+                elif isinstance(value, (dict, list)):
+                    visit(predicate, extractor, value)
+        else:
+            for value in tree:
+                visit(predicate, extractor, value)
 
 
 def process(data):
