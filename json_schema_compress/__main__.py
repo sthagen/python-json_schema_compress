fe0fc471ade1ae8093a610b8eb2f129cb2cdfed9
Stefan Hagen <stefan@hagen.link>
2020-12-25 19:30:48 +0100

    Moved no cover pragma to correct line.

--- json_schema_compress/__main__.py
+++ json_schema_compress/__main__.py
@@ -4,5 +4,5 @@ import sys
 
 from json_schema_compress.cli import main
 
-if __name__ == "__main__":  # pragma: no cover
-    sys.exit(main(sys.argv[1:]))
+if __name__ == "__main__":
+    sys.exit(main(sys.argv[1:]))  # pragma: no cover
